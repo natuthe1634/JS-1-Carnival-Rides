b565cde8660f17d02fad94e5a7818cd65989dc33
natuthe1634 <Princenatu22@gmail.com>
2023-02-24 18:54:06 -0800

    added in if else and a switch statement

--- index.js
+++ index.js
@@ -10,22 +10,32 @@ let waterIsOn = true
 //Everything below is fair game for you to power on the rides
 
 //Ride 1 works when:
+
     // green wire is connected
 ride1PowerOn = false
-
+if (greenWireConnected) {
+    ride1PowerOn = true
+}
 //Ride 2 works when 
     //green and yellow wires are connected
 ride2PowerOn = false
-
+if (greenWireConnected && yellowWireConnected) {
+    ride2PowerOn = true
+}
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 ride3PowerOn = false
-
+if (greenWireConnected && redWireConnected && waterIsOn){
+    ride3PowerOn = true
+}
 //Ride 4 has power when:
     //ride wire is connected
 ride4PowerOn = false
+if (redWireConnected) {
+    ride4PowerOn = true
+}
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -34,6 +44,25 @@ ride4PowerOn = false
     //if a 5 is rolled it should return "bowling ball"
     //if a 6 is rolled it should return "tiger"
     //if a 7 is rolled it should return "seven"
-function ride4SlotMachine(rolledNumber){
-    return "nothing"
-}
+    function ride4SlotMachine(rolledNumber){
+    
+        switch (rolledNumber) {
+            case 1:
+                return "star"
+            case 2:
+                return "car"
+            case 3:
+                return "pot of gold"
+            case 4:
+                return "clover"
+            case 5:
+                return "bowling ball"
+            case 6:
+                return "tiger"
+            case 7:
+                return "seven"
+            default:
+                break;
+        }
+    }
+    
